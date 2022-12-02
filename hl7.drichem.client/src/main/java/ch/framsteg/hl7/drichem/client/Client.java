474a37ad28f0d339f2164c68ef3a3c324ae01891
Olivier Debenath <olivier.debenath@framsteg.ch>
2022-12-02 12:41:07 +0100

    Added running increment of drichem client

---
+++ hl7.drichem.client/src/main/java/ch/framsteg/hl7/drichem/client/Client.java
@@ -0,0 +1,91 @@
+package ch.framsteg.hl7.drichem.client;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] data) {
+
+		// data is taken as command line argument
+		String ipAddress = data[0];
+		int portNumber = Integer.parseInt(data[1]);
+		// int rollNumber = Integer.parseInt(data[2]);
+		// String name = data[3];
+		// String gender = data[4];
+
+		char END_OF_BLOCK = '\u001c';
+		char START_OF_BLOCK = '\u000b';
+		char CARRIAGE_RETURN = 13;
+		int END_OF_TRANSMISSION = -1;
+
+		StringBuffer testHL7Message = new StringBuffer();
+
+		testHL7Message.append(START_OF_BLOCK).append(
+				"MSH|^~\\&||4711|\"\"|\"\"|20200108155010||OUL^R22^OUL_R22|1|P|2.5.1||||||UNICODE UTF-8|||LAB-29^IHE")
+				.append(CARRIAGE_RETURN)
+				.append("SPM|1|41||SER^Serum^HL70487|||||||P^Patient^HL70369\n" + "SAC|||123456799||||||||||||1^A^L")
+				.append(CARRIAGE_RETURN).append("OBR||||U^Unspecified identifier^L").append(CARRIAGE_RETURN)
+				.append("ORC|SC||||CM").append(CARRIAGE_RETURN)
+				.append("OBX|1|NM|GLU^JC10||105|mg/dL^^UCUM|73-109|\"\"|||F|||||4711||SN0123456789|20200108153601")
+				.append(CARRIAGE_RETURN).append("OBX|1|ST|MDT^GLU^JC10||123456,2018/12,  #        ,1,0,0||||||F")
+				.append(CARRIAGE_RETURN).append("TCD|GLU^JC10|^1^:^1||||||O^Original, first run^HL70389")
+				.append(CARRIAGE_RETURN)
+				.append("OBX|2|NM|TCHO^JC10||150|mg/dL^^UCUM|\"\"|\"\"|||F|||||4711||SN0123456789|20200108153601")
+				.append(CARRIAGE_RETURN).append("OBX|2|ST|MDT^TCHO^JC10||223344,2020/12,           ,1,0,0||||||F")
+				.append(CARRIAGE_RETURN)
+				.append("TCD|3F050000002329101^TCHO^JC10|^1^:^1||||||O^Original, first run^HL70389")
+				.append(CARRIAGE_RETURN)
+				.append("OBX|3|NM|HDL-C^JC10||50|mg/dL^^UCUM|\"\"|\"\"|||F|||||4711||SN0123456789|20200108153601")
+				.append(CARRIAGE_RETURN).append("OBX|3|ST|MDT^HDL-C^JC10||234567,2020/12,           ,1,0,0||||||F")
+				.append(CARRIAGE_RETURN)
+				.append("TCD|3F070000002329101^HDL-C^JC10|^1^:^1||||||O^Original, first run^HL70389")
+				.append(CARRIAGE_RETURN)
+				.append("OBX|4|NM|non-HDL-C^JC10||100|mg/dL^^UCUM|\"\"|\"\"|||F|||||4711||SN0123456789|20200108153601")
+				.append(CARRIAGE_RETURN).append("OBX|4|ST|MDT^non-HDL-C^JC10||,,           ,1,0,0||||||F")
+				.append(CARRIAGE_RETURN)
+				.append("TCD|3F069000002391901^non-HDL-C^JC10|^1^:^1||||||O^Original, first run^HL70389")
+				.append(CARRIAGE_RETURN).append(END_OF_BLOCK).append(CARRIAGE_RETURN);
+
+		try {
+			Socket socket = new Socket(ipAddress, portNumber);
+			OutputStream outputStream;
+			OutputStreamWriter outputStreamWriter;
+			InputStream inputStream;
+			InputStreamReader inputStreamReader;
+			StringBuffer stringBuffer;
+			String response;
+			int x;
+
+			// retrieving output Stream and its writer, for sending request or
+			// acknowledgement
+			outputStream = socket.getOutputStream();
+			outputStreamWriter = new OutputStreamWriter(outputStream);
+			outputStreamWriter.write(testHL7Message.toString());
+			// outputStream.write(testHL7Message.toString().getBytes());
+			outputStreamWriter.flush(); // request is sent
+			// retrieving input stream and its reader, for receiving acknowledgement or
+			// response
+
+			inputStream = socket.getInputStream();
+			byte[] byteBuffer = new byte[200];
+
+			inputStream.read(byteBuffer);
+			inputStreamReader = new InputStreamReader(inputStream);
+			stringBuffer = new StringBuffer();
+			System.out.println("Here we are");
+			System.out.println("Received from Server: ");
+			System.out.println(new String(byteBuffer));
+			socket.close(); // closing the connection
+
+		} catch (Exception exception) {
+			// Raised in case, connection is refused or some other technical issue
+			System.out.println(exception);
+		}
+
+	}
+
+}
